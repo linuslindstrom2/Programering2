bcaece1f17564458dfc1e808546cab6c0890bda0
linuslindstrom2 <54801743+linuslindstrom2@users.noreply.github.com>
2020-08-28 14:05:49 +0200

    uppdate

--- HelloNiklas/Program.cs
+++ HelloNiklas/Program.cs
@@ -7,7 +7,36 @@ namespace HelloNiklas
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Niklas!");
+
+            Console.WriteLine("What's your name?");
+            string name = Console.ReadLine();
+            
+            Console.WriteLine("Hello " + name + ", how old are you?");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            string alive;
+
+            if (age > 20)
+            {
+                Console.WriteLine("Dam, you're old...");
+                Console.WriteLine("You sure you're alive?");
+                alive = Console.ReadLine();
+            }
+            else 
+            {
+                Console.WriteLine("Nice, you're quite young!");
+                Console.WriteLine("Then you're still alive?");
+                alive = Console.ReadLine();
+            }
+
+            if (alive == "yes")
+            {
+                Console.WriteLine("Happy to hear man.");
+            }
+            else if (alive == "no")
+            {
+                Console.WriteLine("Dam, sorry man...");
+            }
         }
     }
 }
-
