61f2e550a888f15aec29c1f5c29dd1fbe7a9519b
linuslindstrom2 <54801743+linuslindstrom2@users.noreply.github.com>
2020-09-11 15:07:15 +0200

    g

---
+++ Listor och Arrayer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Listor_och_Arrayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in 5 namn.");
+            string[] names = new string[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                names[i] = Console.ReadLine();
+            }
+
+            Console.WriteLine("Du skrev in dessa namnen, fast i motsatt årdning:");
+
+            for (int i = 4; i >= 0; i--)
+            {
+                Console.WriteLine(names[i]);
+            }
+        }
+    }
+}
