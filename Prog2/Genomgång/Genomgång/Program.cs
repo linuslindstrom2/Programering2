cf068abe2a152d14c268bcab26d769bd46459bcb
linuslindstrom2 <54801743+linuslindstrom2@users.noreply.github.com>
2020-09-21 11:48:02 +0200

    jh

---
+++ Prog2/Genomgång/Genomgång/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Genomgång
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Prog2/Genomgång/Genomgång2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Genomgång2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
