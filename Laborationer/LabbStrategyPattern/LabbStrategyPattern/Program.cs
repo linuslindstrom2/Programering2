83e971a5688f057faa4f05dc34397fadd36c2c78
linuslindstrom2 <54801743+linuslindstrom2@users.noreply.github.com>
2021-02-01 12:48:05 +0100

    h

---
+++ Laborationer/LabbStrategyPattern/LabbStrategyPattern/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LabbStrategyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("What car do you want to buy?");
+                Console.WriteLine("(1) - Lexus");
+                Console.WriteLine("(2) - Tesla");
+                Console.WriteLine("(3) - Audi");
+                Console.WriteLine("(4) - Porsche");
+
+                var choice = Console.ReadLine();
+
+               
+                String colourOfCar;
+                String colourRims;
+
+
+
+                
+            }
+        }
+    }
+}
