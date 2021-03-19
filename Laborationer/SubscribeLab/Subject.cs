fda82082bda019fe17fef2873325073f660a9615
linuslindstrom2 <54801743+linuslindstrom2@users.noreply.github.com>
2021-03-19 15:20:59 +0100

    subscribers

---
+++ Laborationer/SubscribeLab/Subject.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SubscribeLab
+{
+    class Subject : ISubject
+    {
+        List<Observer> subscribers= new List<Observer>();
+
+
+
+        public void Add(Observer observer)
+        {
+            subscribers.Add(observer);
+        }
+        public void Remove(Observer observer)
+        {
+            subscribers.Remove(observer);
+        }
+        public void Notify()
+        {
+            foreach (var observer in subscribers)
+            {
+                observer.Update();
+            }
+
+        }
+    }
+}
